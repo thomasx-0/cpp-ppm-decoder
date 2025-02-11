ec8d665aaa9b8edc4a18e053d35fadd39ca4975d
Thomas Pennnant <pennantt@icloud.com>
2025-02-11 13:34:04 -0500

    refactor PPM constructor for improved initialization syntax

--- include/PPM.h
+++ include/PPM.h
@@ -6,13 +6,12 @@
 class PPM {
 public:
     std::vector<std::vector<int>> imageData;
-    PPM() {
-        imageData = {
-            {0xFF0000, 0x00FF00, 0x0000FF},
-            {0xFFFFFF, 0x000000, 0x888888},
-            {0x000000, 0xFFFFFF, 0x000000}
-        };
-    }
+
+    PPM() : imageData({
+        {0xFF0000, 0x00FF00, 0x0000FF},
+        {0xFFFFFF, 0x000000, 0x888888},
+        {0x000000, 0xFFFFFF, 0x000000}
+    }) {}
 };
 
 #endif // PPM_H
